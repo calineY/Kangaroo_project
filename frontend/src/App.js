f09cdf0a0789c0582e261bb7d543493db23fa159
Caline <calineyammine123@gmail.com>
2022-04-26 17:30:31 +0300

    Refactored files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Landing from './pages/Landing';
+import Landing from './pages/Landing/Landing';
 
 
 function App() {

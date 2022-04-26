ffb5a606b67128fc17ee6606338cdf8c9196a4f5
Caline <calineyammine123@gmail.com>
2022-04-26 20:48:45 +0300

    Linked get surveys api

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,8 +1,28 @@
-import React from 'react'
 import './styles.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function Table() {
+  const [surveys,setSurveys]=useState([]);
+  
+  const getSurveys = async () => {
+    const url = "http://127.0.0.1:8000/api/surveys";
+    try {
+      const response = await axios.get(url);
+      const dataFetched =response.data;
+      setSurveys(dataFetched);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSurveys();
+  },[]);
+
+ 
+
   return (
     <table className='table_container'>
       <thead className='table_head'>
@@ -12,14 +32,12 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Paris</td>
-          <td>XX1</td>
+        {surveys.map((survey,index)=>
+        <tr key={index}>
+          <td>{survey.name}</td>
+          <td>{survey.code}</td>
         </tr>
-        <tr>
-          <td>Paris</td>
-          <td>XX1</td>
-        </tr>
+       )} 
       </tbody>
   </table>
   )

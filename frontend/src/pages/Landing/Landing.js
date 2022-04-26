a811c29f3fc2d844268996de7080f35672ff1c9f
Caline <calineyammine123@gmail.com>
2022-04-26 18:36:05 +0300

    Imported table component in Landing page

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './styles.css'
+import Table from '../../components/Table/Table'
 
 function Landing() {
   return (
     <div className='container'>
         <div className='title'>Kangaroo surveys</div>
+        <Table/>
     </div>
   )
 }
